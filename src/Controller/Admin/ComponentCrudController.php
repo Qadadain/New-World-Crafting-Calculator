8f94e71a23ebb5230808ad8b3abeab23f552235c
Quentin ADADAIN <61509041+Qadadain@users.noreply.github.com>
2022-02-20 22:53:54 +0100

    Add VichUploader and image field's for component (#10)

--- src/Controller/Admin/ComponentCrudController.php
+++ src/Controller/Admin/ComponentCrudController.php
@@ -7,8 +7,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ComponentCrudController extends AbstractCrudController
 {
@@ -23,6 +25,12 @@ class ComponentCrudController extends AbstractCrudController
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('name', 'Nom'),
+            TextField::new('imageFile', 'Upload')
+                ->setFormType(VichImageType::class)
+                ->onlyOnForms(),
+            ImageField::new('imageName', 'Fichier')
+                ->setBasePath('/images/ingredient/')
+                ->hideOnForm(),
             AssociationField::new('recipe', 'Recette'),
             AssociationField::new('tradeSkill', 'Métier'),
             SlugField::new('slug', 'Slug')->setTargetFieldName('name')
